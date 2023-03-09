4c4384f72dca927c7bb8c5ee9e4f14721d0cc2fd
Luiz Paulo <cpd.luiz.paulo@outlook.com>
2023-03-09 11:31:30 -0300

    Commit com tudo funcionando antes da crianção da função para monitoramento da internet.

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import speedtest
 from PySide2.QtCore import QThread
 import time
 
+
+# Classe que trabalha com Thread, que é a execução de funções sem travar o programa. Se quiser que uma função seja
+# executada sem travar o sistema, deve-se criar uma isntância dessa classe para rodar a função.
 class SpeedTestThread(QThread):
     def __init__(self):
         QThread.__init__(self)
@@ -18,7 +21,6 @@ class SpeedTestThread(QThread):
     def set_function(self, func, param=None):
         self.func = func
         self.param = param
-
 
     def run(self):
         if self.param == None:
@@ -32,6 +34,7 @@ class SpeedTestThread(QThread):
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
+
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
@@ -40,19 +43,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.obtendo_lista_servidores()
 
-        self.operadora_txt = ""
-
-
-
-
         # Se clicado, o botão de selecionar servidor chama uma função que libera o combobox e o botão ok pra testar
         # Com o servidor escolhido
         self.btn_selecionar_servidor.clicked.connect(self.enable_with_btn_seleciona_servidor)
+
+    def monitora_internet(self):
 
 
     # Função que roda a primeira vez o speedtest na chamada do programa
     def first_test(self):
 
+        self.lb_teste_de_velocidade_loading.setText("Executando primeiro teste...")
         speedtester = speedtest.Speedtest()
         server = speedtester.get_best_server()
         download_speed = speedtester.download()
@@ -72,9 +73,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.lb_operadora_velocidade.setText(str(self.operadora_txt))
 
         self.btn_selecionar_servidor.setEnabled(True)
+        self.btn_monitora_internet.setEnabled(True)
 
-        return True
-
+        self.lb_teste_de_velocidade_loading.setText("Primeiro teste concluído!")
 
     # Função para obter a lista dos servidores disponíveis para o teste de velocidade
     def obtendo_lista_servidores(self):
@@ -92,7 +93,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # e logo após criando uma lista com o nome do servidor para ser usado no combobox
         for key in self.servidores_speedtest_json:
             for dicionario_valor in self.servidores_speedtest_json[key]:
-                self.lista_servidores_nomes_ids.append({"Nome: ": dicionario_valor['sponsor'], "id": dicionario_valor['id']})
+                self.lista_servidores_nomes_ids.append(
+                    {"Nome: ": dicionario_valor['sponsor'], "id": dicionario_valor['id']})
                 self.lista_servidores_nomes.append((str(dicionario_valor['sponsor'])))
 
         # Usando a lista de nomes de servidores obtida pela iteração acima para aparecer na combobox
@@ -100,8 +102,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # Inicia a função que está entre parenteses quando o botão for clicado
         self.btn_testar_internet_servidor_selecionado.clicked.connect(self.test_with_selected_server)
-
-
 
     # Função na chamada do botão para testar com o servidor selecinado
     def test_with_selected_server(self):
@@ -117,20 +117,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             if server['Nome: '] == self.texto_atual_cb:
                 self.id_servidor_escolhido = server['id']
 
-        # self.test_speed(self.id_servidor_escolhido)
-
+        # Instância da classe SpeedTestThread para executar a função test_speed com parâmetro do ID do servidor.
+        #######################################################################
         self.bloco = SpeedTestThread()  #
         self.bloco.set_function(self.test_speed, self.id_servidor_escolhido)  #
-        self.bloco.start()
-
-
+        self.bloco.start()  #
+        #######################################################################
 
     # Função que será chamada quando já tiver sido feita uma escolha de servidor
     def test_speed(self, servidor):
+        self.btn_testar_internet_servidor_selecionado.setDisabled(True)
+        self.cb_servidores.setDisabled(True)
+        self.lb_teste_de_velocidade_loading.setText("Testando com servidor selecionado...")
         speedtester = speedtest.Speedtest()
         # servers = speedtester.get_servers()
         # print(servers)
 
+        # Laço que pega o id do servidor passado para essa função (test_speed) como parâmetro e extrai todo o
+        # dicionário referente a esse servidor, para passar pra função get_best_server, da classe speedtester, que
+        # setará como servidor de testes o servidor escolhido
         for ping, servers in self.servidores_speedtest_json.items():
             for server in servers:
                 if server['id'] == servidor:
@@ -156,6 +161,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.lb_servidor_velocidade.setText(str(self.server_txt))
         self.lb_operadora_velocidade.setText(str(self.operadora_txt))
 
+        self.btn_testar_internet_servidor_selecionado.setEnabled(True)
+        self.cb_servidores.setEnabled(True)
+        self.lb_teste_de_velocidade_loading.setText("Teste com servidor selecionado concluido!")
+
     # Função para testar a conexão com a ‘internet’
     def check_internet_connection(self):
         url = 'https://www.google.com/'
@@ -164,27 +173,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             _ = requests.get(url, timeout=timeout)
             print('Conexão de internet bem-sucedida')
             self.img_status_da_internet.setPixmap('./images/sinal_verde.png')
-            self.lb_online_offline.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; font-weight:600;\">Online</span></p></body></html>", None))
-            self.btn_monitora_internet.setEnabled(True)
+            self.lb_online_offline.setText(QCoreApplication.translate("MainWindow",
+                                                                      u"<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; font-weight:600;\">Online</span></p></body></html>",
+                                                                      None))
 
-           # Criando thread para exdcutar a função first_test
+            # Criando thread para exdcutar a função first_test
             ##############################################
-            self.bloco = SpeedTestThread()               #
-            self.bloco.set_function(self.first_test)     #
-            self.bloco.start()                           #
+            self.bloco = SpeedTestThread()  #
+            self.bloco.set_function(self.first_test)  #
+            self.bloco.start()  #
             ##############################################
             return True
 
         except requests.ConnectionError:
             print('Sem conexão de internet disponível.')
             self.img_status_da_internet.setPixmap('./images/sinal_vermelho.png')
-            self.lb_online_offline.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; font-weight:600;\">Offline</span></p></body></html>", None))
+            self.lb_online_offline.setText(QCoreApplication.translate("MainWindow",
+                                                                      u"<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; font-weight:600;\">Offline</span></p></body></html>",
+                                                                      None))
             self.btn_monitora_internet.setEnabled(True)
             return False
 
-
     # Função acionada pelo butão de selecionar servidor. Libera os o combobox com os servidores disponíveis e o botão ok
     def enable_with_btn_seleciona_servidor(self):
+        self.btn_selecionar_servidor.setDisabled(True)
         self.btn_testar_internet_servidor_selecionado.setEnabled(True)
         self.cb_servidores.setEnabled(True)
 
